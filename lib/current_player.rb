542a571b36731eaacaeb376201428bdfbd556f1c
David Anderson <matandersonlv@hotmail.com>
2018-08-28 19:38:40 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,19 @@
-def current_player()
+def turn_count(board)
+  counter = 0
+  tokens = ["X", "O"]
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
+  counter
+end
 
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+else
+  turn_count(board).odd?
+    "O"
+  end
 end
